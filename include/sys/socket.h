a1c9870a7e149c01734b2d38ef95dd94ad19c765
Zack W. <unicornfart@disroot.org>
2025-03-09 20:58:29 -0400

    Same as the last commit.  Merely for compatibility.

--- include/sys/socket.h
+++ include/sys/socket.h
@@ -355,7 +355,7 @@ struct linger {
 
 #define CMSG_DATA(cmsg) ((unsigned char *) (((struct cmsghdr *)(cmsg)) + 1))
 #define CMSG_NXTHDR(mhdr, cmsg) ((cmsg)->cmsg_len < sizeof (struct cmsghdr) || \
-	__CMSG_LEN(cmsg) + sizeof(struct cmsghdr) >= __MHDR_END(mhdr) - (unsigned char *)(cmsg) \
+	__CMSG_LEN(cmsg) + sizeof(struct cmsghdr) >= (size_t)(__MHDR_END(mhdr) - (unsigned char *)(cmsg)) \
 	? 0 : (struct cmsghdr *)__CMSG_NEXT(cmsg))
 #define CMSG_FIRSTHDR(mhdr) ((size_t) (mhdr)->msg_controllen >= sizeof (struct cmsghdr) ? (struct cmsghdr *) (mhdr)->msg_control : (struct cmsghdr *) 0)
 
@@ -371,11 +371,7 @@ struct sockaddr {
 	char sa_data[14];
 };
 
-struct sockaddr_storage {
-	sa_family_t ss_family;
-	char __ss_padding[128-sizeof(long)-sizeof(sa_family_t)];
-	unsigned long __ss_align;
-};
+#include <bits/sockaddr_storage.h>
 
 int socket (int, int, int);
 int socketpair (int, int, int, int [2]);
